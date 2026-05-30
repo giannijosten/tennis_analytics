509c451d4af310076af6ef5fc505af8f7d7b1f47
Gianni Josten <josten.giannijakob@fh-swf.de>
2026-05-30 17:20:37 +0200

    style: Kopf-Kommentare in CAPS

--- tennis_analytics/items.py
+++ tennis_analytics/items.py
@@ -2,26 +2,26 @@ import scrapy
 
 
 class TennisItem(scrapy.Item):
-    # --- Basis-Informationen ---
+    # --- BASIS-INFORMATIONEN ---
     name = scrapy.Field()               # Vollständiger Produkt-Name
     brand = scrapy.Field()              # Marke des Produkts
     category = scrapy.Field()           # Kategorie ('rackets', 'shoes', 'strings')
     gender = scrapy.Field()             # Damen, Herren, Unisex
     reference_variant = scrapy.Field()  # L2/L3, 39/43, 1.25 mm
 
-    # --- Identifikator ---
+    # --- IDENTIFIKATOR ---
     ean = scrapy.Field()                # Europäische Artikelnummer
 
-    # --- Preis-Daten ---
+    # --- PREIS-DATEN ---
     current_price = scrapy.Field()      # Der aktuell angezeigte Preis
     regular_price = scrapy.Field()      # gestrichener Preis
     msrp_price = scrapy.Field()         # UVP
     currency = scrapy.Field()           # Währung
 
-    # --- Verfügbarkeit ---
+    # --- VERFÜGBARKEIT ---
     availability = scrapy.Field()       # Lagerstatus ('in_stock', 'out_of_stock')
 
-    # --- Metadaten ---
+    # --- METADATEN ---
     retailer = scrapy.Field()           # Daten-Herkunft ('Tennis-Point', 'Tennistown', 'Tennis-Heine')
     url = scrapy.Field()                # Link zum Produkt zur Validierung
     timestamp = scrapy.Field()          # Exakter Zeitpunkt des Crawls
