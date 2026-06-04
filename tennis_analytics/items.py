ed2ec658d83b419a43d9ba67a4e3664dc7f68ab0
Gianni Josten <josten.giannijakob@fh-swf.de>
2026-06-04 18:31:22 +0200

    style: Anordnung der Felder gemäß Datenspeicherung angepasst

--- tennis_analytics/items.py
+++ tennis_analytics/items.py
@@ -2,11 +2,15 @@ import scrapy
 
 
 class TennisItem(scrapy.Item):
+    # --- METADATEN ---
+    retailer = scrapy.Field()           # Daten-Herkunft ('Tennis-Point', 'Tennistown', 'Tennis-Heine')
+    timestamp = scrapy.Field()          # Exakter Zeitpunkt des Crawls
+
     # --- BASIS-INFORMATIONEN ---
-    name = scrapy.Field()               # Vollständiger Produkt-Name
-    brand = scrapy.Field()              # Marke des Produkts
     category = scrapy.Field()           # Kategorie ('rackets', 'shoes', 'strings')
     gender = scrapy.Field()             # Damen, Herren, Unisex
+    brand = scrapy.Field()              # Marke des Produkts
+    name = scrapy.Field()               # Vollständiger Produkt-Name
     reference_variant = scrapy.Field()  # L2/L3, 39/43, 1.25 mm
 
     # --- IDENTIFIKATOR ---
@@ -21,7 +25,5 @@ class TennisItem(scrapy.Item):
     # --- VERFÜGBARKEIT ---
     availability = scrapy.Field()       # Lagerstatus ('in_stock', 'out_of_stock')
 
-    # --- METADATEN ---
-    retailer = scrapy.Field()           # Daten-Herkunft ('Tennis-Point', 'Tennistown', 'Tennis-Heine')
+    # --- QUELLENNACHWEIS --- 
     url = scrapy.Field()                # Link zum Produkt zur Validierung
-    timestamp = scrapy.Field()          # Exakter Zeitpunkt des Crawls
